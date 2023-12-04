99fdbb7ed975b0b677a18666fc567b9453d926ec
Akilarasan <akilarasan20@gmail.com>
2023-12-04 11:33:50 +0530

    home page creating

--- Login/urls.py
+++ Login/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     
     path("employee_login/", views.employee_login, name = "employee_login"),
+    path("employee_logout/", views.employee_logout, name = "employee_logout"),
     
-    path("patient_login/", views.patient_login, name ="patient_login")]
+    path("patient_login/", views.patient_login, name ="patient_login"),
+    path("patient_logout/", views.patient_logout, name ="patient_logout")
+    ]
 
 
--- Login/views.py
+++ Login/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+# HOME PAGE
 def home(request):
     return render(request, 'base/home.html')
 
+
+# EMPLOYEE LOGIN
 def employee_login(request):
     return render(request, 'base/employee_login.html')
 
+
+def employee_logout(request):
+    return redirect('home')
+
+
+#PATIENT LOGIN
 def patient_login(request):
     return render(request, 'base/patient_login.html')
+
+def patient_logout(request):
+    return redirect('home')
